889f38c48feafd99dd2730a6fe72745d05d4b88c
Paul Kasidit <paulkasidit@gmail.com>
2023-01-07 12:33:53 -0800

    push changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,6 @@ import ticketsImage from "./../img/tickets.png";
 function Header(){
   return (
     <React.Fragment>
-      <h1>Help Queue</h1>
       <img src={ticketsImage} alt="An image of tickets" />
     </React.Fragment>
   );
