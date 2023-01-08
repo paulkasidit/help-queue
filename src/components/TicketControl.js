9e0d795d60b773c5344219de038befc0133d0dd1
Paul Kasidit <paulkasidit@gmail.com>
2023-01-08 13:13:24 -0800

    push changes

--- src/components/TicketControl.js
+++ src/components/TicketControl.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import NewTicketForm from './NewTicketForm';
+import TicketDetail from './TicketDetail';
 import TicketList from './TicketList';
 
 class TicketControl extends React.Component {
@@ -7,34 +8,63 @@ class TicketControl extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      formVisibleOnPage: false
+      formVisibleOnPage: false,
+      mainTicketList: [],
+      selectedTicket: null 
     };
   }
 
+  handleAddingNewTicketToList = (newTicket) => {
+    const newMainTicketList = this.state.mainTicketList.concat(newTicket);
+    this.setState({mainTicketList: newMainTicketList,
+                  formVisibleOnPage: false });
+  }
+
+  handleChangingSelectedTicket = (id) => {
+    const selectedTicket = this.state.mainTicketList.filter(ticket => ticket.id === id)[0];
+    this.setState({selectedTicket: selectedTicket});
+  }
+
   handleClick = () => {
-    this.setState(prevState => ({
-      formVisibleOnPage: !prevState.formVisibleOnPage
-    }));
+    if (this.state.selectedTicket != null) {
+      this.setState({
+        formVisibleOnPage: false,
+        selectedTicket: null
+      });
+    } else {
+      this.setState(prevState => ({
+        formVisibleOnPage: !prevState.formVisibleOnPage,
+      }));
+    }
   }
 
   render(){
     let currentlyVisibleState = null;
-    let buttonText = null;
-    if (this.state.formVisibleOnPage) {
-      currentlyVisibleState = <NewTicketForm />
+    let buttonText = null; 
+
+    if (this.state.selectedTicket != null) {
+      currentlyVisibleState = <TicketDetail ticket = {this.state.selectedTicket} />
+      buttonText = "Return to Ticket List";
+      // While our TicketDetail component only takes placeholder data, we will eventually be passing the value of selectedTicket as a prop.
+    }
+    else if (this.state.formVisibleOnPage) {
+      // This conditional needs to be updated to "else if."
+      currentlyVisibleState = <NewTicketForm onNewTicketCreation={this.handleAddingNewTicketToList}  />;
       buttonText = "Return to Ticket List";
     } else {
-      currentlyVisibleState = <TicketList />
+      currentlyVisibleState = <TicketList ticketList={this.state.mainTicketList} onTicketSelection={this.handleChangingSelectedTicket} />;
+      // Because a user will actually be clicking on the ticket in the Ticket component, we will need to pass our new handleChangingSelectedTicket method as a prop.
       buttonText = "Add Ticket";
     }
+
     return (
       <React.Fragment>
         {currentlyVisibleState}
-        <button onClick  = {this.handleClick}>{buttonText}</button>
-      </ React.Fragment>
+        <button onClick={this.handleClick}>{buttonText}</button>
+      </React.Fragment>
     );
   }
-
+  
 }
 
 export default TicketControl;
--- src/components/TicketList.js
+++ src/components/TicketList.js
@@ -1,41 +1,31 @@
 import React from "react";
 import Ticket from "./Ticket";
+import PropTypes from "prop-types";
 
-const mainTicketList = [
-  {
-    names: 'Thato and Haley',
-    location: '3A',
-    issue: 'Firebase won\'t save record. Halp.'
-  },
-  {
-    names: 'Sleater and Kinney',
-    location: '4B',
-    issue: 'Prop types are throwing an error.'
-  },
-  {
-    names: 'Imani & Jacob',
-    location: '9F',
-    issue: 'Child component isn\'t rendering.'
-  },
-  {
-    names: 'Normani & Sam Smith', 
-    location: 'London', 
-    issue: 'Dancing With a Stranger'
-  }
-];
+// remove const mainTicketList = [ ... ]. We no longer want these.
 
-function TicketList(){
-  return(
+function TicketList(props) { // Add props as parameter.
+
+  return (
     <React.Fragment>
       <hr/>
-      {mainTicketList.map((ticket,index) => 
-        <Ticket names = {ticket.names} 
-          location = {ticket.location}
-          issue = {ticket.issue} 
-          key = {index}/>
+      {props.ticketList.map((ticket) => 
+        <Ticket whenTicketClicked = {props.onTicketSelection}
+        names = {ticket.names}
+        location = {ticket.location}
+        issue = {ticket.issue}
+        id = {ticket.id}
+        key = {ticket.id}
+        />
       )}
     </React.Fragment>
-  );
+  )
 }
+
+// Add propTypes for ticketList.
+TicketList.propTypes = {
+  ticketList: PropTypes.array,
+  onTicketSelection: PropTypes.func
+};
 
 export default TicketList;
